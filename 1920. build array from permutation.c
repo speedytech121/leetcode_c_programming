11d7b3636b1bb266e38c5d61477547bb9e3a9d8a
speedytech121 <deepak.tag2@gmail.com>
2021-08-17 21:26:10 +0530

    commited

---
+++ 1920. build array from permutation.c
@@ -0,0 +1,16 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize){
+    
+    int* ans=(int *)malloc(numsSize*sizeof(int));
+    for(int i=0;i<numsSize;i++){
+        ans[i]=nums[nums[i]];
+    }
+    *returnSize=numsSize;
+    return ans;
+    free(nums);
+    
+}
